7d33c3a507710fe8a290518ec19a688942d92d77
zhongyongsheng <itvincent@gmail.com>
2023-02-08 12:03:13 +0800

    doc(vite):add comment

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import legacy from '@vitejs/plugin-legacy'
 
 export default {
   plugins: [
+    // see more: https://github.com/vitejs/vite/tree/main/packages/plugin-legacy
     legacy({
         targets: ['ie >= 11'],
         additionalLegacyPolyfills: ['regenerator-runtime/runtime'],
